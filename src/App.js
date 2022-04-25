bee8ce341b8d04807eb8db29189ea59f0a0650ac
Ruby Roy <rubyroy722@gmail.com>
2022-04-26 00:39:29 +0530

    scroll chalta jaiga

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const PAGE_NUMBER = 1
+
 function App() {
+  const [state, setState] = useState([])
+
+  const [page, setPage] = useState(PAGE_NUMBER)
+
+  useEffect(() => {
+    fetch(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=5`)
+    .then(res => res.json())
+    .then(json => setState([...state, ...json.data]))
+  }, [page])
+
+  const scrollToEnd = () => {
+    setPage(page + 1)
+  }
+
+  window.onscroll = function () {
+
+    if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+      scrollToEnd()
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.length > 0 && state.map((el, i) => (
+        <div key={i} className='container'>
+          <h4>Id: {el.id}</h4>
+          <h4>Name: {el.name}</h4>
+          <h4>Trips: {el.trips}</h4>
+        </div>
+      ))}
     </div>
   );
 }
